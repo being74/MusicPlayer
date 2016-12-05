3b62f3aee04479f3bcd7b77763db78bac31d3d7f
xuqiang <coderxuqiang@gmail.com>
2016-12-05 19:21:36 +0800

    add

---
+++ app/src/main/java/com/music/qiang/musicplayer/ui/fragment/MusicListFragment.java
@@ -0,0 +1,191 @@
+package com.music.qiang.musicplayer.ui.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.music.qiang.musicplayer.R;
+import com.music.qiang.musicplayer.model.MusicFile;
+import com.music.qiang.musicplayer.ui.activity.MusicPlayActivity;
+import com.music.qiang.musicplayer.ui.adapter.MusicListAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MusicListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MusicListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MusicListFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    // ****************Views*******************
+    private View rootView;
+    private RecyclerView rv_music_list;
+    private Toolbar toolbar;
+
+    // ****************对象********************
+    private Context mContext;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private MusicListAdapter musicListAdapter;
+    private ArrayList<MusicFile> musicFiles;
+
+    public MusicListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MusicListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MusicListFragment newInstance(String param1, String param2) {
+        MusicListFragment fragment = new MusicListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_music_list, container, false);
+        mContext = getActivity();
+        initViews();
+        initData();
+
+        return rootView;
+    }
+
+    private void initViews() {
+        rv_music_list = (RecyclerView) rootView.findViewById(R.id.rv_content_main_music_list);
+    }
+
+    private void initData() {
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        // 1. 创建线性LayoutManager
+        mLayoutManager = new LinearLayoutManager(mContext);
+        rv_music_list.setLayoutManager(mLayoutManager);
+        // 2. 如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        rv_music_list.setHasFixedSize(true);
+        // 3. 创建并设置适配器
+        getMusicList();
+        musicListAdapter = new MusicListAdapter(musicFiles);
+        rv_music_list.setAdapter(musicListAdapter);
+        // 4. 设置点击事件
+        musicListAdapter.setOnItemClickListener(new MusicListAdapter.MyItemClickListener() {
+            @Override
+            public void onItemClickListener(View view, int position) {
+                //Toast.makeText(MainActivity.this, "第" + position + "个", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(mContext, MusicPlayActivity.class));
+            }
+
+            @Override
+            public void onItemLongClickListener(View view, int position) {
+
+            }
+        });
+    }
+
+    /**
+     * 获得媒体文件
+     */
+    private void getMusicList() {
+        musicFiles = new ArrayList<>();
+        Cursor cursor = mContext.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null,
+                MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            MusicFile file = new MusicFile();
+            file.musicId = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID));
+            file.musicName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+            file.musicAlbum = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM));
+            file.musicArtist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
+            file.musicPath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+            file.musicSize = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE));
+            file.musicTime = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION));
+            musicFiles.add(file);
+            cursor.moveToNext();
+        }
+        cursor.close();
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        /*if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
