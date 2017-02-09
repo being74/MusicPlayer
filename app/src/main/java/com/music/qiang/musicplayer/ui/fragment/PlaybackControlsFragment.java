6ad0f954e57a84ddedbefd7055d03ceb3aee0add
xuqiang <coderxuqiang@gmail.com>
2017-02-09 18:44:32 +0800

    ..

---
+++ app/src/main/java/com/music/qiang/musicplayer/ui/fragment/PlaybackControlsFragment.java
@@ -0,0 +1,235 @@
+package com.music.qiang.musicplayer.ui.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.music.qiang.musicplayer.R;
+import com.music.qiang.musicplayer.support.utils.LogHelper;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PlaybackControlsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PlaybackControlsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlaybackControlsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private static final String TAG = LogHelper.makeLogTag(PlaybackControlsFragment.class);
+    private OnFragmentInteractionListener mListener;
+    private String mArtUrl;
+    // Receive callbacks from the MediaController. Here we update our state such as which queue
+    // is being shown, the current title and description and the PlaybackState.
+    private final MediaControllerCompat.Callback mCallback = new MediaControllerCompat.Callback() {
+        @Override
+        public void onPlaybackStateChanged(@NonNull PlaybackStateCompat state) {
+            LogHelper.d(TAG, "Received playback state change to state ", state.getState());
+            PlaybackControlsFragment.this.onPlaybackStateChanged(state);
+        }
+
+        @Override
+        public void onMetadataChanged(MediaMetadataCompat metadata) {
+            if (metadata == null) {
+                return;
+            }
+            LogHelper.d(TAG, "Received metadata state change to mediaId=",
+                    metadata.getDescription().getMediaId(),
+                    " song=", metadata.getDescription().getTitle());
+            PlaybackControlsFragment.this.onMetadataChanged(metadata);
+        }
+    };
+
+    public PlaybackControlsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PlayBackFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PlaybackControlsFragment newInstance(String param1, String param2) {
+        PlaybackControlsFragment fragment = new PlaybackControlsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_play_back, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        /*if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    public void onConnected() {
+        MediaControllerCompat controller = ((FragmentActivity) getActivity())
+                .getSupportMediaController();
+        LogHelper.d(TAG, "onConnected, mediaController==null? ", controller == null);
+        if (controller != null) {
+            onMetadataChanged(controller.getMetadata());
+            onPlaybackStateChanged(controller.getPlaybackState());
+            controller.registerCallback(mCallback);
+        }
+    }
+
+    private void onMetadataChanged(MediaMetadataCompat metadata) {
+        LogHelper.d(TAG, "onMetadataChanged ", metadata);
+        if (getActivity() == null) {
+            LogHelper.w(TAG, "onMetadataChanged called when getActivity null," +
+                    "this should not happen if the callback was properly unregistered. Ignoring.");
+            return;
+        }
+        if (metadata == null) {
+            return;
+        }
+
+        /*mTitle.setText(metadata.getDescription().getTitle());
+        mSubtitle.setText(metadata.getDescription().getSubtitle());
+        String artUrl = null;
+        if (metadata.getDescription().getIconUri() != null) {
+            artUrl = metadata.getDescription().getIconUri().toString();
+        }
+        if (!TextUtils.equals(artUrl, mArtUrl)) {
+            mArtUrl = artUrl;
+            Bitmap art = metadata.getDescription().getIconBitmap();
+            AlbumArtCache cache = AlbumArtCache.getInstance();
+            if (art == null) {
+                art = cache.getIconImage(mArtUrl);
+            }
+            if (art != null) {
+                mAlbumArt.setImageBitmap(art);
+            } else {
+                cache.fetch(artUrl, new AlbumArtCache.FetchListener() {
+                            @Override
+                            public void onFetched(String artUrl, Bitmap bitmap, Bitmap icon) {
+                                if (icon != null) {
+                                    LogHelper.d(TAG, "album art icon of w=", icon.getWidth(),
+                                            " h=", icon.getHeight());
+                                    if (isAdded()) {
+                                        mAlbumArt.setImageBitmap(icon);
+                                    }
+                                }
+                            }
+                        }
+                );
+            }
+        }*/
+    }
+
+    private void onPlaybackStateChanged(PlaybackStateCompat state) {
+        LogHelper.d(TAG, "onPlaybackStateChanged ", state);
+        if (getActivity() == null) {
+            LogHelper.w(TAG, "onPlaybackStateChanged called when getActivity null," +
+                    "this should not happen if the callback was properly unregistered. Ignoring.");
+            return;
+        }
+        if (state == null) {
+            return;
+        }
+        boolean enablePlay = false;
+        switch (state.getState()) {
+            case PlaybackStateCompat.STATE_PAUSED:
+            case PlaybackStateCompat.STATE_STOPPED:
+                enablePlay = true;
+                break;
+            case PlaybackStateCompat.STATE_ERROR:
+                LogHelper.e(TAG, "error playbackstate: ", state.getErrorMessage());
+                Toast.makeText(getActivity(), state.getErrorMessage(), Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        /*if (enablePlay) {
+            mPlayPause.setImageDrawable(
+                    ContextCompat.getDrawable(getActivity(), R.drawable.ic_play_arrow_black_36dp));
+        } else {
+            mPlayPause.setImageDrawable(
+                    ContextCompat.getDrawable(getActivity(), R.drawable.ic_pause_black_36dp));
+        }
+
+        MediaControllerCompat controller = ((FragmentActivity) getActivity())
+                .getSupportMediaController();
+        String extraInfo = null;
+        if (controller != null && controller.getExtras() != null) {
+            String castName = controller.getExtras().getString(MusicService.EXTRA_CONNECTED_CAST);
+            if (castName != null) {
+                extraInfo = getResources().getString(R.string.casting_to_device, castName);
+            }
+        }
+        setExtraInfo(extraInfo);*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
