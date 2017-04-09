1b0e1f8c5e081ad61a159c845da299869a953e13
qiang <coderxuqiang@gmail.com>
2017-04-09 22:31:30 +0800

    22:31

--- app/src/main/java/com/music/qiang/musicplayer/ui/fragment/AlbumListFragment.java
+++ app/src/main/java/com/music/qiang/musicplayer/ui/fragment/AlbumListFragment.java
@@ -1,109 +1,137 @@
 package com.music.qiang.musicplayer.ui.fragment;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.music.qiang.musicplayer.R;
+import com.music.qiang.musicplayer.model.AlbumFile;
+import com.music.qiang.musicplayer.model.MusicFile;
+import com.music.qiang.musicplayer.ui.activity.MusicPlayActivity;
+import com.music.qiang.musicplayer.ui.adapter.AlbumListAdapter;
+import com.music.qiang.musicplayer.ui.adapter.MusicListAdapter;
+
+import java.util.ArrayList;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AlbumListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AlbumListFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * 专辑列表fragment
  */
 public class AlbumListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // ****************Views*******************
+    private View rootView;
+    private RecyclerView albumlistView;
 
-    private OnFragmentInteractionListener mListener;
+    // ****************对象********************
+    private Context mContext;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private AlbumListAdapter albumListAdapter;
+    private ArrayList<AlbumFile> albumFiles;
 
     public AlbumListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlbumListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlbumListFragment newInstance(String param1, String param2) {
-        AlbumListFragment fragment = new AlbumListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album_list, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        rootView = inflater.inflate(R.layout.fragment_album_list, container, false);
+        mContext = getActivity();
+        initViews();
+        initData();
+        return rootView;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        /*if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
+
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+    }
+
+    private void initViews() {
+        albumlistView = (RecyclerView) rootView.findViewById(R.id.rv_fragment_album_list);
+    }
+
+    private void initData() {
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        // 1. 创建线性LayoutManager
+        mLayoutManager = new LinearLayoutManager(mContext);
+        albumlistView.setLayoutManager(mLayoutManager);
+        // 2. 如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        albumlistView.setHasFixedSize(true);
+        // 3. 创建并设置适配器
+        getAlbumList();
+        albumListAdapter = new AlbumListAdapter(mContext, albumFiles);
+        albumlistView.setAdapter(albumListAdapter);
+        // 4. 添加分割线
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(albumlistView.getContext(),
+                LinearLayout.VERTICAL);
+        albumlistView.addItemDecoration(dividerItemDecoration);
+
+        // 5. 设置点击事件
+        albumListAdapter.setOnItemClickListener(new AlbumListAdapter.MyItemClickListener() {
+            @Override
+            public void onItemClickListener(View view, int position) {
+                /*Intent intent = new Intent(mContext, MusicPlayActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("from", "list");
+                bundle.putInt("playIndex", position);
+                bundle.putSerializable("playList", albumFiles);
+                intent.putExtras(bundle);
+                startActivity(intent);*/
+            }
+
+            @Override
+            public void onItemLongClickListener(View view, int position) {
+
+            }
+        });
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     * 获得媒体文件
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void getAlbumList() {
+        albumFiles = new ArrayList<>();
+        Cursor cursor = mContext.getContentResolver().query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, null, null, null,
+                MediaStore.Audio.Albums.DEFAULT_SORT_ORDER);
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            AlbumFile file = new AlbumFile();
+            file.albumId = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums._ID));
+            file.albumName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM));
+            file.albumArtist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums.ARTIST));
+            file.albumSongNumber = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums.NUMBER_OF_SONGS));
+            file.albumKey = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM_KEY));
+
+            albumFiles.add(file);
+            cursor.moveToNext();
+        }
+        cursor.close();
     }
 }
