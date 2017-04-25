744e68ee15ea1f9039719aacda9984b93247df04
qiang <coderxuqiang@gmail.com>
2017-04-25 22:44:46 +0800

    add http request

--- app/src/main/java/com/music/qiang/musicplayer/ui/fragment/MoreFragment.java
+++ app/src/main/java/com/music/qiang/musicplayer/ui/fragment/MoreFragment.java
@@ -2,65 +2,69 @@ package com.music.qiang.musicplayer.ui.fragment;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.music.qiang.musicplayer.R;
+import com.music.qiang.musicplayer.support.utils.HttpUtil;
+
+import java.io.IOException;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link MoreFragment#newInstance} factory method to
- * create an instance of this fragment.
+ *
  */
 public class MoreFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View rootView;
+    private TextView test;
+    private String response = "";
 
-
-    public MoreFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MoreFragment newInstance(String param1, String param2) {
-        MoreFragment fragment = new MoreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Handler mHandler = new Handler();
+    private final Runnable updateUi = new Runnable() {
+        @Override
+        public void run() {
+            test.setText(response);
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        rootView = inflater.inflate(R.layout.fragment_more, container, false);
+        test = (TextView) rootView.findViewById(R.id.test);
+        initData();
+        return rootView;
     }
 
+    private void initData() {
+        //URL u=new URL("http://route.showapi.com/213-4?showapi_appid=myappid&topid=&showapi_sign=mysecret");
+        final String url = "http://route.showapi.com/213-4?showapi_appid=" + 35608 + "&topid=5&showapi_sign=dd559b3a18ee485fa098924d3df792ab";
+
+
+        new Thread() {
+            //在新线程中发送网络请求
+            public void run() {
+                try {
+                    response = HttpUtil.run(url);
+                    mHandler.post(updateUi);
+                    Log.d("xuqiang", response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+
+    }
 }
