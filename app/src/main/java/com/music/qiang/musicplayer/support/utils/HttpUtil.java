901db8643001bfffeaf348a198aee0a7da10d26f
xuqiang <coderxuqiang@gmail.com>
2017-05-04 18:44:59 +0800

    add AsyncHttpClient

--- app/src/main/java/com/music/qiang/musicplayer/support/utils/HttpUtil.java
+++ app/src/main/java/com/music/qiang/musicplayer/support/utils/HttpUtil.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import java.io.IOException;
 
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -19,18 +22,42 @@ import okhttp3.Response;
 
 public class HttpUtil {
 
-    private static OkHttpClient client = new OkHttpClient();
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
+    private static OkHttpClient okhttpClient = new OkHttpClient();
+    //public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
+    private static String BASE_URL = "";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    /**
+     * okhttp的get请求
+     * @param url
+     * @return
+     * @throws IOException
+     */
     public static String run(String url) throws IOException {
         Request request = new Request.Builder().url(url).build();
-        Response response = client.newCall(request).execute();
+        Response response = okhttpClient.newCall(request).execute();
         if (response.isSuccessful()) {
             return response.body().string();
         } else {
             throw new IOException("Unexpected code " + response);
         }
+    }
+
+    public static void doGet(String url, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), responseHandler);
+    }
+
+    public static void doGet(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void doPost(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
     }
 
     /**
